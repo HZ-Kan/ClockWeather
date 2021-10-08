2a0b9905d2510703b017833ce7ed463862b4d34c
khzsczl88 <1046973290@qq.com>
2021-10-08 22:44:40 +0800

    添加CW样式

--- src/pages/ClockWeather/ClockWeather.vue
+++ src/pages/ClockWeather/ClockWeather.vue
@@ -1,6 +1,45 @@
 <template>
-	
+    <div class="CW">
+        <div style="--i:1;" class="CW_container">
+	        <span>7</span>
+        </div>
+        <div style="--i:2;" class="CW_container">
+	        <span>8</span>
+        </div>
+        <div style="--i:3;" class="CW_container">
+	        <span>9</span>
+        </div>
+        <div style="--i:4;" class="CW_container"> 
+	        <span>10</span>
+        </div>
+        <div style="--i:5;" class="CW_container">
+	        <span>11</span>
+        </div>
+        <div style="--i:6;" class="CW_container">
+	        <span>12</span>
+        </div>
+        <div style="--i:7;" class="CW_container">
+	        <span>1</span>
+        </div>
+        <div style="--i:8;" class="CW_container">
+	        <span>2</span>
+        </div>
+        <div style="--i:9;" class="CW_container">
+	        <span>3</span>
+        </div>
+        <div style="--i:10;" class="CW_container">
+	        <span>4</span>
+        </div>
+        <div style="--i:11;" class="CW_container">
+	        <span>5</span>
+        </div>
+        <div style="--i:12;" class="CW_container">
+	        <span>6</span>
+        </div>
+        <div class="clock">00:00:00</div>
+    </div>
 </template>
+
 
 <script>
 	export default {
@@ -15,6 +54,44 @@
 	}
 </script>
 
-<style>
+<style scoped>
+.CW{
+    width: 80vw;
+    height: 80vw;
+    position: relative;
+    background: #eee;
+    border-radius: 50%;
+}
+.CW .CW_container{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    color: white;
+    font-size: 6vw;
+    text-shadow: 0 0 5px black;
+}
+.CW span{
+    position: absolute;
+    transform: rotate(0);
+
+}
+.CW .CW_container{
+    transform: rotateZ(calc(var(--i) * 30deg)) translateY(40vw);
+}
+.CW .CW_container span{
+    transform: rotateZ(calc(var(--i) * -30deg));
+}
+
+.clock{
+    position: absolute;
+    font-size: 6vw;
+    left: 50%;
+    top: 60%;
+    transform: translateX(-50%);
+    color: #1e90ff;
+    background: black;
+    padding: 1.5vw;
+    border-radius: 1.5vw;
+}
 	
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,23 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
+
+		<ClockWeather />
+
 	</view>
 </template>
-
+ 
 <script>
+
+	import ClockWeather from '../ClockWeather/ClockWeather.vue';
 	export default {
+
+		components:{
+			ClockWeather
+		},
 		data() {
+			
 			return {
-				title: 'Hello'
+				
 			}
 		},
 		onLoad() {
@@ -24,26 +30,10 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.content{
+	margin-top: 50vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 </style>
